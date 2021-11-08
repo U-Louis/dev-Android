dbd691f6744e32d22ae9d05de5cae09deba75409
Louis <69362039+U-Louis@users.noreply.github.com>
2021-11-08 12:52:22 +0100

    +menu

--- rrr5/app/src/main/java/m/lou/rrr5/MainActivity.java
+++ rrr5/app/src/main/java/m/lou/rrr5/MainActivity.java
@@ -3,14 +3,19 @@ package m.lou.rrr5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     public Card selectedCard;
 
@@ -80,6 +85,25 @@ public class MainActivity extends AppCompatActivity {
 
             //setup adapter
 
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.newgame:
+                //TODO
+                Toast.makeText(this, "new game", Toast.LENGTH_SHORT).show();
+             return true;
+            default:
+                return false;
+            }
+    }
+
+    public void showMenu(View v){
+        PopupMenu menu = new PopupMenu(this, v);
+        menu.setOnMenuItemClickListener(this);
+        menu.inflate(R.menu.mainmenu);
+        menu.show();
 
     }
 
